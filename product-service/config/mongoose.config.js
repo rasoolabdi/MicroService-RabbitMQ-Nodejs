a9529d19f88cf2fc3522cad55ee7119be713f8bf
rasoolabdi <M0rd00r@yahoo.com>
2024-07-26 14:26:21 +0330

    edit mongoose config product

--- product-service/config/mongoose.config.js
+++ product-service/config/mongoose.config.js
@@ -1,7 +1,7 @@
 const { default: mongoose } = require("mongoose");
 
 
-mongoose.connect("mongodb://localhost:3003/MicroRabbit-Product").then(() => {
+mongoose.connect("mongodb://localhost:27017/MicroRabbit-Product").then(() => {
     console.log("connected to product service")
 }).catch(() => {
     console.log("connected to product service Failed")
